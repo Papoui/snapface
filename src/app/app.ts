9d6246c7d44c394b6e41f8b51f019319c542909d
Pap <darckrim64@gmail.com>
2026-05-13 12:46:37 +0200

    Affichez des listes

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,15 +11,31 @@ import { FaceSnapM } from './models/face-snap';
   styleUrl: './app.scss'
 })
 export class App implements OnInit{
-  mySnap!: FaceSnapM;
+  faceSnaps!: FaceSnapM[];
 
   ngOnInit() {
-    this.mySnap = new FaceSnapM(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
+    this.faceSnaps = [
+      new FaceSnapM(
+        'Archibald',
+        'Mon meilleur ami depuis toujours !',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        10
+      ),
+      new FaceSnapM(
+        'Three Rock Mountain',
+        'Un endroit magnifique pour les randonnées.',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        new Date(),
+        6
+      ),
+      new FaceSnapM(
+        'Un bon repas',
+        'Mmmh que c\'est bon !',
+        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        new Date(),
+        156
+      )
+    ];
   }
 }
