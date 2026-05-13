fe98e624b21634a6f3873ce0862fa1aef87922f9
Pap <darckrim64@gmail.com>
2026-05-13 12:40:57 +0200

    conditionnez l'affichage des éléments

--- src/app/models/face-snap.ts
+++ src/app/models/face-snap.ts
@@ -1,7 +1,14 @@
 export class FaceSnapM {
+
+  location?: string;
+
   constructor(public title: string,
               public description: string,
               public imageUrl: string,
               public createdAt: Date,
               public snaps: number) {}
+  
+  setlocation(location: string): void {
+    this.location = location;
+  }
 }
