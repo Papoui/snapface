a7f45d4275f4e3e96adebda49377845176739205
Pap <darckrim64@gmail.com>
2026-05-15 17:20:42 +0200

    passez en SPA avec le routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list';
+import { LandingPageComponent } from './components/landing-page-component/landing-page-component';
 
 export const routes: Routes = [
-    {path : '', component : FaceSnapListComponent}
+    {path : '', component : LandingPageComponent},
+    {path : 'facesnaps', component : FaceSnapListComponent}
 ];
